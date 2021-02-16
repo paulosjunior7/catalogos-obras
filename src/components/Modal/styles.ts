21f09ab93209d10d54bccbad8d4fe4598c2e8abe
paulosjunior7 <paulosjunior7@gmail.com>
2021-02-16 16:52:17 -0300

    master

--- src/components/Modal/styles.ts
+++ src/components/Modal/styles.ts
@@ -7,8 +7,8 @@ export const Background  = styled.div`
     background-color: rgba(0,0,0,0.3);
     display: flex;
     position: fixed;
-    width: 100%;
-    height: 100%;
+    width: 100vw;
+    height: 100vh;
     top: 0;
     left: 0;
     z-index: 999;
