b2da997972c09d67dbf834d629629cae922cc7cc
Paulo Soares <paulosjunior7@gmail.com>
2022-10-03 18:18:00 -0300

    ajuste casas

--- src/utils/residencias.ts
+++ src/utils/residencias.ts
@@ -43,48 +43,6 @@ import OBRA1802_5 from '../Assets/OBRA1802/OBRA1802_5.jpeg';
 import OBRA1802_6 from '../Assets/OBRA1802/OBRA1802_6.jpeg';
 import OBRA1802_7 from '../Assets/OBRA1802/OBRA1802_7.jpeg';
 
-import OBRA3205_1 from '../Assets/OBRA3205/OBRA3205_1.jpeg';
-import OBRA3205_2 from '../Assets/OBRA3205/OBRA3205_2.jpeg';
-import OBRA3205_3 from '../Assets/OBRA3205/OBRA3205_3.jpeg';
-import OBRA3205_4 from '../Assets/OBRA3205/OBRA3205_4.jpeg';
-import OBRA3205_5 from '../Assets/OBRA3205/OBRA3205_5.jpeg';
-import OBRA3205_6 from '../Assets/OBRA3205/OBRA3205_6.jpeg';
-import OBRA3205_7 from '../Assets/OBRA3205/OBRA3205_7.jpeg';
-import OBRA3205_8 from '../Assets/OBRA3205/OBRA3205_8.jpeg';
-import OBRA3205_9 from '../Assets/OBRA3205/OBRA3205_9.jpeg';
-import OBRA3205_10 from '../Assets/OBRA3205/OBRA3205_10.jpeg';
-import OBRA3205_11 from '../Assets/OBRA3205/OBRA3205_11.jpeg';
-import OBRA3205_12 from '../Assets/OBRA3205/OBRA3205_12.jpeg';
-import OBRA3205_13 from '../Assets/OBRA3205/OBRA3205_13.jpeg';
-import OBRA3205_14 from '../Assets/OBRA3205/OBRA3205_14.jpeg';
-import OBRA3205_15 from '../Assets/OBRA3205/OBRA3205_15.jpeg';
-import OBRA3205_16 from '../Assets/OBRA3205/OBRA3205_16.jpeg';
-import OBRA3205_17 from '../Assets/OBRA3205/OBRA3205_17.jpeg';
-import OBRA3205_18 from '../Assets/OBRA3205/OBRA3205_18.jpeg';
-import OBRA3205_19 from '../Assets/OBRA3205/OBRA3205_19.jpeg';
-import OBRA3205_20 from '../Assets/OBRA3205/OBRA3205_20.jpeg';
-
-import OBRA3621_1 from '../Assets/OBRA3621/OBRA3621_1.jpeg';
-import OBRA3621_2 from '../Assets/OBRA3621/OBRA3621_2.jpeg';
-import OBRA3621_3 from '../Assets/OBRA3621/OBRA3621_3.jpeg';
-import OBRA3621_4 from '../Assets/OBRA3621/OBRA3621_4.jpeg';
-import OBRA3621_5 from '../Assets/OBRA3621/OBRA3621_5.jpeg';
-import OBRA3621_6 from '../Assets/OBRA3621/OBRA3621_6.jpeg';
-import OBRA3621_7 from '../Assets/OBRA3621/OBRA3621_7.jpeg';
-import OBRA3621_8 from '../Assets/OBRA3621/OBRA3621_8.jpeg';
-import OBRA3621_9 from '../Assets/OBRA3621/OBRA3621_9.jpeg';
-import OBRA3621_10 from '../Assets/OBRA3621/OBRA3621_10.jpeg';
-import OBRA3621_11 from '../Assets/OBRA3621/OBRA3621_11.jpeg';
-import OBRA3621_12 from '../Assets/OBRA3621/OBRA3621_12.jpeg';
-import OBRA3621_13 from '../Assets/OBRA3621/OBRA3621_13.jpeg';
-import OBRA3621_14 from '../Assets/OBRA3621/OBRA3621_14.jpeg';
-import OBRA3621_15 from '../Assets/OBRA3621/OBRA3621_15.jpeg';
-import OBRA3621_16 from '../Assets/OBRA3621/OBRA3621_16.jpeg';
-import OBRA3621_17 from '../Assets/OBRA3621/OBRA3621_17.jpeg';
-import OBRA3621_18 from '../Assets/OBRA3621/OBRA3621_18.jpeg';
-import OBRA3621_19 from '../Assets/OBRA3621/OBRA3621_19.jpeg';
-import OBRA3621_20 from '../Assets/OBRA3621/OBRA3621_20.jpeg';
-
 import OBRAB15_1 from '../Assets/OBRAB15/OBRAB15_1.jpeg';
 import OBRAB15_2 from '../Assets/OBRAB15/OBRAB15_2.jpeg';
 import OBRAB15_3 from '../Assets/OBRAB15/OBRAB15_3.jpeg';
@@ -100,53 +58,6 @@ import OBRAB15_12 from '../Assets/OBRAB15/OBRAB15_12.jpg';
 import OBRAB15_13 from '../Assets/OBRAB15/OBRAB15_13.jpg';
 
 export const Casas = [
-  {
-    id: 3,
-    endereco: "Avenida Parque, quadra 32, lote 05, CASA 2",
-    bairro: "Três Marias",
-    cidade: "Goiânia",
-    valor: "R$ 350.000,00",
-    status: "Concluida",
-    previsaoConclusao: "Março/2022",
-    detalhes: [
-      "3 Quartos, sendo 1 suíte",
-      "Banheiro Social",
-      "Sala com cozinha ampla integrada ",
-      "Area de Serviço coberta",
-      "Garagem coberta ",
-      "Varanda Gourmet com churrasqueira",
-      "Piso Porcelanato",
-      "Acabamento alto padrão"
-    ],
-    condicoes: "Financiamento bancário ou a vista",
-    areaConstruida: "117 m²",
-    lote: "",
-    fracaolote: "180 m²",
-    quartos: 3,
-    banheiros: 2,
-    imagens: [
-      OBRA3205_1,
-      OBRA3205_2,
-      OBRA3205_3,
-      OBRA3205_4,
-      OBRA3205_5,
-      OBRA3205_6,
-      OBRA3205_7,
-      OBRA3205_8,
-      OBRA3205_9,
-      OBRA3205_10,
-      OBRA3205_11,
-      OBRA3205_12,
-      OBRA3205_13,
-      OBRA3205_14,
-      OBRA3205_15,
-      OBRA3205_16,
-      OBRA3205_17,
-      OBRA3205_18,
-      OBRA3205_19,
-      OBRA3205_20
-    ]
-  },
   {
     id: 1,
     endereco: "Avenida Presidente Dutra, quadra 06, lote 11, Casa 1 e Casa 2",
@@ -252,65 +163,13 @@ export const Casas = [
     ]
   },
   {
-    id: 4,
-    endereco: "Rua MDV 27 , quadra 36, lote 21, Casa 1 e Casa 2",
-    bairro: "Moinho dos Ventos",
-    cidade: "Goiânia",
-    valor: "R$ 450.000,00",
-    status: "Concluida",
-    previsaoConclusao: "Julho/2022",
-    detalhes: [
-      "3 Quartos, sendo 1 suíte",
-      "Banheiro Social",
-      "Sala ",
-      "Cozinha",
-      "Area de Serviço coberta",
-      "Garagem coberta ",
-      "Varanda Gourmet com churrasqueira",
-      "Piscina com pontos de hidro, led e cascata",
-      "Lavabo",
-      "Piso Porcelanato",
-      "Esquadrias em aluminio",
-      "Bancada cozinha marmore crema kalahary",
-      "Acabamento alto padrão"
-    ],
-    condicoes: "Financiamento bancário ou a vista",
-    areaConstruida: "111 m²",
-    lote: "",
-    fracaolote: "180 m²",
-    quartos: 3,
-    banheiros: 3,
-    imagens: [
-      OBRA3621_1,
-      OBRA3621_2,
-      OBRA3621_3,
-      OBRA3621_4,
-      OBRA3621_5,
-      OBRA3621_6,
-      OBRA3621_7,
-      OBRA3621_8,
-      OBRA3621_9,
-      OBRA3621_10,
-      OBRA3621_11,
-      OBRA3621_12,
-      OBRA3621_13,
-      OBRA3621_14,
-      OBRA3621_15,
-      OBRA3621_16,
-      OBRA3621_17,
-      OBRA3621_18,
-      OBRA3621_19,
-      OBRA3621_20
-    ]
-  },
-  {
     id: 5,
     endereco: "Avenida Presidente Epitacio Pessoa, quadra B, lote 15",
     bairro: "Jardim Presidente Extensão III",
     cidade: "Goiânia",
     valor: "R$ 1.000.000,00",
     status: "Em_Construção",
-    previsaoConclusao: "Outubro/2022",
+    previsaoConclusao: "Novembro/2022",
     detalhes: [
       "3 suítes",
       "sendo 1 suíte master com Banheira de hidromassagem e closet",
